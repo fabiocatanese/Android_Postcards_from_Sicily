4aac2ff71890e07a7d7c5b3345d5da9f4e0f866f
Fabio Catanese <Fabio Catanese>
2017-02-21 17:53:40 +0100

    *first commit*

---
+++ app/src/main/java/com/example/android/sicily/Sicily_postcard.java
@@ -0,0 +1,13 @@
+package com.example.android.sicily;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sicily_postcard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sicily_postcard);
+    }
+}
